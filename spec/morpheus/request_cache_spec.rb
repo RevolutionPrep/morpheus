d9b2d73b10faff2341e16ef0466b5e271853b92d
Ryan Moran <ryan.moran@gmail.com>
2011-12-23 19:48:31 -0800

    coverage for request cache

--- spec/morpheus/request_cache_spec.rb
+++ spec/morpheus/request_cache_spec.rb
@@ -1,17 +1,36 @@
 require 'spec_helper'
 
 describe Morpheus::RequestCache do
+  let(:app) { mock(:call => nil) }
+  let(:cache) { Morpheus::RequestCache.new(app) }
 
   describe '#initialize' do
-    pending
+    it 'takes an app instance and sets its instance variable' do
+      obj = Object.new
+      cache = Morpheus::RequestCache.new(obj)
+      cache.instance_variable_get('@app').should eql(obj)
+    end
   end
 
   describe '#call' do
-    pending
+    it 'clears the cache hash' do
+      Morpheus::RequestCache.cache.should_receive(:clear)
+      cache.call(Object.new)
+    end
+
+    it 'delegates to the app with the env' do
+      env = Object.new
+      app.should_receive(:call).with(env)
+      cache.call(env)
+    end
   end
 
   describe '.cache' do
-    pending
+    it 'returns the global cache hash' do
+      cache_hash = Morpheus::RequestCache.cache
+      cache_hash.should be_kind_of(Hash)
+      cache_hash.should be_empty
+    end
   end
 
 end
